157c3aae137607b4ffed92e729f92a96bc034dc8
BorisKarl <herrnielsen@web.de>
2024-02-29 13:45:01 +0100

    Game starts with eventListener, trying to position the name of the player on top of the board

--- src/index.js
+++ src/index.js
@@ -1,24 +1,43 @@
 import Ship from "./modules/Ship";
-import { displayBoard } from "./modules/UI";
+import { displayBoard, displayHeader, displayText, makePlayer } from "./modules/UI";
 import GameBoard from "./modules/Gameboard";
 
-const cocaine = new Ship(5, "Cocaine");
-const meth = new Ship(4, "Meth");
-const crack = new Ship(3, "Crack");
-const weed = new Ship(2, "Weed");
-const shrooms = new Ship(2, "Shrooms");
 
-const human_board = new GameBoard("human_board");
-const machine_board = new GameBoard("machine_board");
+displayHeader();
 
-const arrayOfShips = [cocaine, meth, crack, weed, shrooms];
-arrayOfShips.forEach((e) =>{
-    human_board.placeShips(e);
-    machine_board.placeShips(e);
+const button = document.getElementById("button");
+
+// Start Game
+button.addEventListener("click", () => {
+    let human = makePlayer("Human");
+    const header = document.getElementById("header");
+    header.style.color = "green";
+
+    displayText(human.name);
+
+    // Define "Ships" aka drugs
+    const cocaine = new Ship(5, "Cocaine");
+    const meth = new Ship(4, "Meth");
+    const crack = new Ship(3, "Crack");
+    const weed = new Ship(2, "Weed");
+    const shrooms = new Ship(2, "Shrooms");
+
+     displayText(crack.name);
+    // Make new boards
+    const human_board = new GameBoard("human_board");
+    const machine_board = new GameBoard("machine_board");
+
+    // Set ships with array
+    const arrayOfShips = [cocaine, meth, crack, weed, shrooms];
+    arrayOfShips.forEach((e) => {
+      human_board.placeShips(e);
+      machine_board.placeShips(e);
+    });
+
+    console.log(human_board, machine_board);
+
+    displayBoard("human");
+    displayBoard("machine");
 });
 
-console.log(machine_board);
-
-displayBoard("gameboard_human");
-displayBoard("gameboard_machine");
 
--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,22 +1,60 @@
 import { GameBoard } from "./Gameboard.js";
+import { Player } from "./Player.js";
+
+const displayHeader = () => {
+    const body = document.querySelector("body");
+    const header = document.createElement("h1");
+    const p = document.createElement("p");
+    const button = document.createElement("button");
+    header.setAttribute("id", "header");
+    header.textContent = "Drug Run";
+    p.textContent = "Play against the biggest druglords and become the next Kingpin of Rotterdam!";
+    button.setAttribute("id", "button");
+    button.textContent = "START";
+    header.appendChild(p);
+    header.appendChild(button);
+    body.insertBefore(header, body.firstChild);
+}
+
+const makePlayer = (name) => {
+        const player = new Player(name);
+        return player;
+}
+
+const displayText = (text) => {
+    const p = document.createElement("p");
+    const body = document.querySelector("body");
+    p.textContent = text;
+    body.appendChild(p);
+}
+
+const displayPlayerName = (name) => {
+    const nameDiv = document.createElement("div");
+}
 
 function displayBoard(id) {
+    const player_h2 = document.createElement("h2");
+    player_h2.textContent = id;
     const content = document.getElementById("content");
+    const wrapper = document.createElement("span");
+    wrapper.style.backgroundColor ="red";
     const board_one = document.createElement("div");
     board_one.setAttribute("id", id);
     const gameBoard = new GameBoard();
     const array = gameBoard.buildBoard();
     array.forEach((e) => {
-    let element = document.createElement("div");
-    element.setAttribute("id", e);
-    element.className = id;
-    element.addEventListener("mouseover", (e) => {
-      console.log(e.target.id);
+        let element = document.createElement("div");
+        element.setAttribute("id", e);
+        element.className = id;
+        element.addEventListener("mouseover", (e) => {
+        console.log(e.target.id);
     });
-    board_one.appendChild(element);
+        board_one.appendChild(element);
   });
+  wrapper.insertBefore(player_h2, wrapper.firstChild);
+  content.appendChild(wrapper);
   content.appendChild(board_one);
-  console.log(`Building board ${id}`);
+  console.log(`${id} ready!`);
 }
 
 const showPosition = (ship) => {
@@ -47,4 +85,4 @@ const setShipOnDiv = (coord) => {
   // Eventlistener checkt feld, bei klick wird das Schiff gesetzt
 };
 
-export { displayBoard, showPosition, clickShip };
+export { displayBoard, showPosition, clickShip, displayHeader, displayText, makePlayer };
